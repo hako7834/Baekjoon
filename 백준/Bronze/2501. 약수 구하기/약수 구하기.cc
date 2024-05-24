ffdd22255e2daaec01345dadd8b207e660bfdae0
준우 <167217736+hako7834@users.noreply.github.com>
2024-05-24 16:33:57 +0900

    [Bronze III] Title: 약수 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.cc
@@ -1,25 +1,12 @@
-#include <iostream>
-
-int tmp[10001] = {0,};
-void yaksu(int n) {
-    for (int i = 1; i <= n; i++) {
-        if (!(n%i)) tmp[i] = 1;
-    }
-    // i가 n의 약수면 tmp[i] = 1. 아니면 0
-}
-
-int search(int N, int K) {
-    int cnt = 0;
-    for (int i = 1; i <= N; i++) {
-        if (tmp[i]) cnt++;
-        if (cnt == K) return i;
-    }
-    return 0;
-}
-
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> dp;
 int main() {
     int N, K;
-    std::cin >> N >> K;
-    yaksu(N);
-    std::cout << search(N, K);
+    cin >> N >> K;
+    for (int i = 1; i <= N; i++) {
+        if (!(N%i)) dp.push_back(i);
+    }
+    if (dp.size() >= K) cout << dp[K-1];
+    else cout << 0;
 }
